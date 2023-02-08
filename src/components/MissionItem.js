19cd476db9e9c6a30c6f0fd7987f4b5604094787
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2023-02-08 16:15:40 +0530

    JoinMission LeaveMission ToggleButtons

--- src/components/MissionItem.js
+++ src/components/MissionItem.js
@@ -1,29 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
+import { useDispatch } from 'react-redux';
+import { joinMissionAction, leaveMissionAction } from '../redux/missions/missions';
 
 const MissionItem = (props) => {
-  const { missionName, missionDesc } = props;
+  const {
+    id, missionName, missionDesc, reserved,
+  } = props;
+  const dispatch = useDispatch();
+
+  const clickHandler = (e) => {
+    if (!reserved) {
+      dispatch(joinMissionAction(e.target.parentNode.parentNode.id));
+      return;
+    }
+
+    dispatch(leaveMissionAction(e.target.parentNode.parentNode.id));
+  };
 
   return (
-    <tr>
+    <tr id={id}>
       <td>{missionName}</td>
       <td>{missionDesc}</td>
-      <td width={135}>
-        <Button variant="secondary" className="center" size="sm">NOT A MEMBER</Button>
-        {' '}
+      <td width={150}>
+        { reserved ? <Button variant="primary" className="am-btn" size="sm">ACTIVE MEMBER</Button> : <Button variant="secondary" className="center" size="sm">NOT A MEMBER </Button> }
       </td>
-      <td width={130}>
-        <Button variant="outline-secondary" className="center" size="sm">JOIN MISSION</Button>
-        {' '}
+      <td width={150}>
+        { reserved ? <Button variant="outline-danger" className="center" size="sm" onClick={clickHandler}>LEAVE MISSION</Button> : <Button variant="outline-secondary" className="center" size="sm" onClick={clickHandler}>JOIN MISSION</Button> }
       </td>
     </tr>
   );
 };
 
 MissionItem.propTypes = {
+  id: PropTypes.string.isRequired,
   missionName: PropTypes.string.isRequired,
   missionDesc: PropTypes.string.isRequired,
+  reserved: PropTypes.bool.isRequired,
 };
 
 export default MissionItem;
--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 const FETCH_MISSIONS_ONCE = 'spacehub/missions/FETCH_MISSIONS_ONCE';
+const JOIN_MISSION = 'spacehub/missions/JOIN_MISSION';
+const LEAVE_MISSION = 'spacehub/missions/LEAVE_MISSION';
 
 const url = 'https://api.spacexdata.com/v3/missions';
 
@@ -17,12 +19,32 @@ const fetchMissonsAction = () => async (dispatch) => {
   });
 };
 
+const joinMissionAction = (id) => ({
+  type: JOIN_MISSION,
+  id,
+});
+
+const leaveMissionAction = (id) => ({
+  type: LEAVE_MISSION,
+  id,
+});
+
 export function missionsReducer(state = [], action) {
   switch (action.type) {
     case FETCH_MISSIONS_ONCE:
       return action.missions;
+    case JOIN_MISSION:
+      return state.map((mission) => {
+        if (mission.mission_id !== action.id) return mission;
+        return { ...mission, reserved: true };
+      });
+    case LEAVE_MISSION:
+      return state.map((mission) => {
+        if (mission.mission_id !== action.id) return mission;
+        return { ...mission, reserved: false };
+      });
     default: return state;
   }
 }
 
-export { fetchMissonsAction };
+export { fetchMissonsAction, joinMissionAction, leaveMissionAction };
